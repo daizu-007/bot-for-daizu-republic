4ce9575c58e7e2fc70d2041311b99c188b43d8fb
daizu <daizu007public@gmail.com>
2023-08-04 01:30:51 +0900

    コマンドの追加

--- bot.py
+++ bot.py
@@ -20,8 +20,6 @@ dcToken = data["keys"]["dcToken"]
 #discordのwebhookのURL
 dcURL = data["keys"]["dcURL"]
 
-#実行
-bot.run(dcToken) #botを実行
 
 #botが起動したときの処理
 @bot.event
@@ -42,7 +40,7 @@ async def on_member_join(member):
 async def on_member_remove(member):
     print("member left")
     #退出メッセージを作成
-    message = f"{member.name}さんが、大豆共和国を去りました。"
+    message = f"{member.display_name}さんが、大豆共和国を去りました。"
     #退出メッセージを送信
     await bot.get_channel(1124235204375085108).send(message)
 
@@ -55,12 +53,16 @@ async def on_message(message):
     #メッセージが送信されたチャンネルが自己紹介チャンネルなら
     if message.channel.id == 1124235224641982516:
         #メッセージが自己紹介かどうかを判定
-        if isSelfIntroduction(message.content,message.author.name):
-            #メッセージを送信
-            await message.channel.send("自己紹介ありがとうございます。")
+        if isSelfIntroduction(message.content,message.author.display_name):
+            #国民ロールを付与
+            await message.author.add_roles(message.guild.get_role(1124582468918452225))
+            await message.channel.send(f"{message.author.mention}さん、おめでとうございます。あなたは今から大豆共和国民です！")
         else:
             #メッセージを送信
-            await message.channel.send("自己紹介をお願いします。")
+            await message.channel.send(f"<@1034077539938881577>、{message.author.mention}さんが自己紹介をしました。確認してください。")
+    #メッセージが送信されたチャンネルが自己紹介チャンネルでないなら
+    else:
+        await bot.process_commands(message) #コマンドを処理する
 
 #文章が自己紹介かどうかを判定する関数
 def isSelfIntroduction(text,user):
@@ -69,5 +71,25 @@ def isSelfIntroduction(text,user):
     else:
         return False
 
+#コマンド
+#pingコマンド
+@bot.command(name="ping", description="pingを返します。", guild_ids=["1109024847432007771"])
+async def ping(ctx): 
+    await ctx.send("pong")
 
+#helpコマンド
+@bot.command(name="list", description="コマンド一覧を表示します。", guild_ids=["1109024847432007771"])
+async def help(ctx):
+    #helpメッセージを作成
+    message = """
 
+コマンド一覧
+    /ping:pingを返します。
+    /help:コマンド一覧を表示します。
+
+"""
+    #helpメッセージを送信
+    await ctx.send(message)
+
+#実行
+bot.run(dcToken) #botを実行
