341594dd95f5ab0f783101a31805491a75b2a945
daizu <daizu007public@gmail.com>
2023-08-06 00:38:38 +0900

    カスタムスタンプの送信時に元の発言を削除するように変更

--- bot.py
+++ bot.py
@@ -1,37 +1,71 @@
 #このdiscord botは、大豆共和国の管理をするbotです。botの名前は、大豆共和国警察庁長官です。
-import discord
-import asyncio
-import requests
-import os
-import json
-from os import path
-from discord.ext import commands
 
-#初期設定
+
+
+###必要なライブラリをインポート###
+
+import discord #pycord discord関連のライブラリ
+import asyncio #非同期処理のライブラリ
+import requests #HTTPリクエストを送信するライブラリ
+import os #os関連の処理をするライブラリ
+import json #jsonファイルを扱うライブラリ
+from os import path #pathを扱うライブラリ 
+from discord.ext import commands #discord botを作成するためのライブラリ
+
+
+
+###デバックモード###
+
+#デバックモードを有効にする
+debug = True
+if debug:
+    import tracemalloc #メモリ使用量を計測するライブラリ
+    tracemalloc.start() #メモリ使用量の計測を開始する
+
+
+
+###初期設定###
+
+
+#bot関連
+
 #botの権限 .allで全ての権限を与える。
 intents = discord.Intents.all()
 #botを定義する
 bot = commands.Bot(intents=intents, command_prefix="/")
-#jsonファイルから情報を取得する
-with open(path.join(path.dirname(__file__), "data.json"), "r") as f:
-    data = json.load(f) #dataにjsonファイルの内容を代入
-#discordのトークン
-dcToken = data["keys"]["dcToken"]
-#discordのwebhookのURL
-dcURLs = data["keys"]["webhookURL"]
+
+
+#ファイルの読み込み
+
+#jsonファイルからconfig情報を取得する
+with open(path.join(path.dirname(__file__), "config.json"), "r") as f:
+    config = json.load(f) #configにjsonファイルの内容を代入
 #jsonファイルから絵文字の情報を取得する
 with open(path.join(path.dirname(__file__), "emojis.json"), "r") as f:
     emojis = json.load(f) #emojisにjsonファイルの内容を代入
+
+
+#定数
+
+#discordのトークン
+dcToken = config["keys"]["dcToken"]
+#discordのwebhookのURL
+dcURLs = config["webhookURLs"]
 #headerの設定
 headers = {'Content-Type': 'application/json'}
 
+
+
+###discordイベント###
 
 #botが起動したときの処理
 @bot.event
 async def on_ready():
     print("bot is ready")   #botが起動したことを表示
 
+
 #新しいユーザーが参加したときの処理
+
 @bot.event
 async def on_member_join(member):
     print("new member joined")
@@ -40,7 +74,9 @@ async def on_member_join(member):
     #welcomeメッセージを送信
     await bot.get_channel(1124235204375085108).send(message)
 
+
 #ユーザーがサーバーから退出したときの処理
+
 @bot.event
 async def on_member_remove(member):
     print("member left")
@@ -49,12 +85,15 @@ async def on_member_remove(member):
     #退出メッセージを送信
     await bot.get_channel(1124235204375085108).send(message)
 
+
 #メッセージが送信されたときの処理
+
 @bot.event
 async def on_message(message):
     #メッセージの送信者がbotなら
     if message.author.bot:
         return #何もしない
+    
     #メッセージが送信されたチャンネルが自己紹介チャンネルなら
     if message.channel.id == 1124235224641982516:
         #メッセージが自己紹介かどうかを判定
@@ -65,40 +104,29 @@ async def on_message(message):
         else:
             #メッセージを送信
             await message.channel.send(f"<@1034077539938881577>、{message.author.mention}さんが自己紹介をしました。確認してください。")
+        return #関数を終了する
+    
     #メッセージが送信されたチャンネルが自己紹介チャンネルでないなら
-    else:
-        if message.content.startswith("$"):
-            emoji_name = message.content[1:] #メッセージの先頭の"$"を削除
-            emoji_URL = emojis.get(emoji_name) #絵文字のURLを取得
-            if emoji_URL == None: #絵文字のURLが存在しないなら
-                await message.channel.send("その絵文字は存在しません。") #TODO 今後送信したユーザーにのみ表示されるようにする
-            else: #絵文字のURLが存在するなら
-                username = message.author.display_name #ユーザー名を取得
-                avatar_of_user= message.author.display_avatar #ユーザーのアバターを取得
-                #スタンプの内容を作成
-                stamp_content = {
-                    "username": username,
-                    "avatar_url": str(avatar_of_user), #アバターのURLを文字列に変換 っていうかasset型ってなんだよ
-                    "content": emoji_URL #TODO 今後embedを使うようにする
-                }
-                #スタンプを送信
-                requests.post(dcURLs["emojis"]["hiroba"], json.dumps(stamp_content), headers=headers)
-        await bot.process_commands(message) #コマンドを処理する
+    if message.content.startswith("$"): #メッセージが"$"で始まるなら
+        #絵文字関連の処理をする
+        await emoji(message)
+        return #関数を終了する
+    
+    await bot.process_commands(message) #コマンドを処理する
 
-#文章が自己紹介かどうかを判定する関数
-def isSelfIntroduction(text,user):
-    if user in text:
-        return True
-    else:
-        return False
 
-#コマンド
+
+###コマンド###
+
 #pingコマンド
+
 @bot.command(name="ping", description="pingを返します。", guild_ids=["1109024847432007771"])
 async def ping(ctx): 
     await ctx.send("pong")
 
+
 #HELPコマンド
+
 @bot.command(name="HELP", description="コマンド一覧を表示します。", guild_ids=["1109024847432007771"])
 async def HELP(ctx):
     #helpメッセージを作成
@@ -112,5 +140,45 @@ async def HELP(ctx):
     #helpメッセージを送信
     await ctx.send(message)
 
-#実行
+
+
+###関数###
+
+#文章が自己紹介かどうかを判定する関数
+
+def isSelfIntroduction(text,user):
+    if user in text:
+        return True
+    else:
+        return False
+
+
+#絵文字関連の処理をする関数
+
+async def emoji(message):
+    #必要な情報を取得
+    emoji_name = message.content[1:] #メッセージの先頭の"$"を削除
+    emoji_URL = emojis.get(emoji_name) #絵文字のURLを取得
+    username = message.author.display_name #ユーザー名を取得
+    avatar_of_user= message.author.display_avatar #ユーザーのアバターを取得
+
+    if emoji_URL == None: #絵文字のURLが存在しないなら
+        await message.reply("その絵文字は存在しません。") #メッセージを送信
+    
+    else: #絵文字のURLが存在するなら
+        #スタンプの内容を作成
+        stamp_content = {
+            "username": username,
+            "avatar_url": str(avatar_of_user), #アバターのURLを文字列に変換 っていうかasset型ってなんだよ
+            "content": emoji_URL #TODO 今後embedを使うようにする
+        }
+        #スタンプを送信
+        requests.post(dcURLs["emojis"]["hiroba"], json.dumps(stamp_content), headers=headers)
+        #元のメッセージを削除
+        await message.delete()
+
+
+
+###実行###
+
 bot.run(dcToken) #botを実行
